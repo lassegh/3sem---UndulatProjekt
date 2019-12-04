afa390040b9e208bda7a4c2bac0fed75325f1233
lassegh <lasse.groenbech@gmail.com>
2019-12-04 11:34:36 +0100

    Pepsi mam

--- UndulatWeb1/dist/bundle.js
+++ UndulatWeb1/dist/bundle.js
@@ -105,14 +105,19 @@ module.exports = __webpack_require__.p + "index.htm";
 /***/ (function(module, exports) {
 
 var contentOfAllRecords = document.getElementById("demo");
-function alertFunc() {
+function alertFuncForBur() {
     alert("Tid til at skifte bur!");
-    myStartFunction();
+    myStartFunctionForBur();
+}
+function alertFuncForMad() {
+    alert("Tid at give Pepsi mad og drikke");
+    myStartFunctionForMad();
 }
 (function () {
-    myStartFunction();
+    myStartFunctionForBur();
+    myStartFunctionForMad();
 })();
-function myStartFunction() {
+function myStartFunctionForBur() {
     // Set the date we're counting down to
     var countDownDate = new Date().getTime() + 20000; //604800000; // Det sidste er en uge i milisekunder
     // Update the count down every 1 second
@@ -130,7 +135,29 @@ function myStartFunction() {
         // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
-            alertFunc();
+            alertFuncForBur();
+        }
+    }, 1000);
+}
+function myStartFunctionForMad() {
+    // Set the date we're counting down to
+    var countDownDate = new Date().getTime() + 15000; //604800000; // Det sidste er en uge i milisekunder
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+        // Get today's date and time
+        var now = new Date().getTime();
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        // Display the result in the element with id="demo"
+        contentOfAllRecords.innerHTML = days + "d " + hours + "h " + minutes + "m";
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            alertFuncForMad();
         }
     }, 1000);
 }
--- UndulatWeb1/src/js/index.ts
+++ UndulatWeb1/src/js/index.ts
@@ -1,17 +1,22 @@
 let contentOfAllRecords : HTMLDivElement = <HTMLDivElement>document.getElementById("demo");
 
-function alertFunc() {
+function alertFuncForBur() {
   alert("Tid til at skifte bur!");
-  myStartFunction();
+  myStartFunctionForBur();
+}
+
+function alertFuncForMad() {
+  alert("Tid at give Pepsi mad og drikke");
+  myStartFunctionForMad();
 }
 
 (()=> {
 
-    myStartFunction();
-    
+    myStartFunctionForBur();
+    myStartFunctionForMad();    
 })();
 
-function myStartFunction() {
+function myStartFunctionForBur() {
   // Set the date we're counting down to
   var countDownDate = new Date().getTime()+20000; //604800000; // Det sidste er en uge i milisekunder
 
@@ -34,7 +39,36 @@ function myStartFunction() {
     // If the count down is finished, write some text
     if (distance < 0) {
       clearInterval(x);
-      alertFunc()
+      alertFuncForBur()
+    }
+  }, 
+  1000);
+}
+
+function myStartFunctionForMad() {
+  // Set the date we're counting down to
+  var countDownDate = new Date().getTime()+15000; //604800000; // Det sidste er en uge i milisekunder
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    // Display the result in the element with id="demo"
+    contentOfAllRecords.innerHTML = days + "d " + hours + "h " + minutes + "m";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      alertFuncForMad()
     }
   }, 
   1000);
