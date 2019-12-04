9d5c3f03d468c0c989c917e78ead6ef63d86a01f
lassegh <lasse.groenbech@gmail.com>
2019-12-04 13:44:09 +0100

    autoreload

--- UndulatWeb1/dist/bundle.js
+++ UndulatWeb1/dist/bundle.js
@@ -2085,6 +2085,7 @@ function alertFuncForMad() {
     showAllRecords();
     myStartFunctionForBur();
     myStartFunctionForMad();
+    autoReloadVideoData();
 })();
 function myStartFunctionForBur() {
     // Set the date we're counting down to
@@ -2127,6 +2128,27 @@ function myStartFunctionForMad() {
         if (distance < 0) {
             clearInterval(x);
             alertFuncForMad();
+        }
+    }, 1000);
+}
+function autoReloadVideoData() {
+    // Set the date we're counting down to
+    var countDownDate = new Date().getTime() + 10000; // Det sidste er en uge i milisekunder
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+        // Get today's date and time
+        var now = new Date().getTime();
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            showAllRecords();
+            autoReloadVideoData();
         }
     }, 1000);
 }
--- UndulatWeb1/src/js/index.ts
+++ UndulatWeb1/src/js/index.ts
@@ -26,7 +26,8 @@ function alertFuncForMad() {
 (()=> {
     showAllRecords();
     myStartFunctionForBur();
-    myStartFunctionForMad();    
+    myStartFunctionForMad();  
+    autoReloadVideoData();  
 })();
 
 function myStartFunctionForBur() {
@@ -82,6 +83,33 @@ function myStartFunctionForMad() {
     if (distance < 0) {
       clearInterval(x);
       alertFuncForMad()
+    }
+  }, 
+  1000);
+}
+
+function autoReloadVideoData() {
+  // Set the date we're counting down to
+  var countDownDate = new Date().getTime()+10000; // Det sidste er en uge i milisekunder
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      showAllRecords();
+      autoReloadVideoData();
     }
   }, 
   1000);
