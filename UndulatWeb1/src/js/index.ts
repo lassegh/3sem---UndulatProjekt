81eb6c3c7ca7d850e8b6bed28282b51d1615c161
Kjergaard <KjergaardVFX@gmail.com>
2019-12-04 10:33:16 +0100

    Timer on website + UI Design

--- UndulatWeb1/src/js/index.ts
+++ UndulatWeb1/src/js/index.ts
@@ -1,14 +1,18 @@
 let contentOfAllRecords : HTMLDivElement = <HTMLDivElement>document.getElementById("demo");
 
+function alertFunc() {
+  alert("Tid til at skifte bur!");
+}
+
 (()=> {
 
     myStartFunction();
-
+    
 })();
 
 function myStartFunction() {
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+var countDownDate = new Date("Dec 5, 2019 10:19:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -22,17 +26,15 @@ var x = setInterval(function() {
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
   // Display the result in the element with id="demo"
-  contentOfAllRecords.innerHTML = days + "d " + hours + "h ";
+  contentOfAllRecords.innerHTML = days + "d " + hours + "h " + minutes + "m";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    alertFunc
+    alertFunc()
   }
 }, 1000);
-
-  function alertFunc() {
-    alert("Tid til at skifte bur!");
-  }}
+}
