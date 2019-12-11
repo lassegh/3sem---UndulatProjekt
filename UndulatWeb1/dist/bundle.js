b0c5ca6f3408a032dcf309d1e9698b350a9a01a4
lassegh <lasse.groenbech@gmail.com>
2019-12-11 13:22:22 +0100

    Refactoring

--- UndulatWeb1/dist/bundle.js
+++ UndulatWeb1/dist/bundle.js
@@ -2072,110 +2072,66 @@ __webpack_require__.r(__webpack_exports__);
 var baseUri = "http://lgwebservice.azurewebsites.net/api/starttime";
 var baseUrlForWeather = "http://api.openweathermap.org/data/2.5/weather?q=roskilde&APPID=1a38fe1f59581dcfa8c2121a26de6b86";
 var videoLogs = document.getElementById("videoLogs");
-var contentOfAllRecords = document.getElementById("cleanTimer");
+var contentForCleanTimer = document.getElementById("cleanTimer");
 var contentOfTimerMad = document.getElementById("feedTimer");
 var contentTemp = document.getElementById("temp");
-function alertFuncForBur() {
-    alert("Tid til at skifte bur!");
-    myStartFunctionForBur();
-}
-function alertFuncForMad() {
-    alert("Tid at give Pepsi mad og drikke");
-    myStartFunctionForMad();
-}
 (function () {
     showAllTimeStamps();
-    myStartFunctionForBur();
-    myStartFunctionForMad();
-    autoReloadVideoData();
+    TimerWithAlert(contentForCleanTimer, 345600000, "Tid til at skifte bur!");
+    TimerWithAlert(contentOfTimerMad, 604800000, "Tid at give Pepsi mad og drikke");
+    autoReloadData();
     showWeather();
 })();
-function myStartFunctionForBur() {
+function TimerWithAlert(divTag, milisecondsToRun, textToAlert) {
     // Set the date we're counting down to
-    var countDownDate = new Date().getTime() + 345600000; // Det sidste er 4 dage i milisekunder
-    // Update the count down every 1 second
+    var countDownDate = new Date().getTime() + milisecondsToRun;
     var x = setInterval(function () {
         // Get today's date and time
         var now = new Date().getTime();
-        // Find the distance between now and the count down date
         var distance = countDownDate - now;
-        // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        // Display the result in the element with id="demo"
-        contentOfAllRecords.innerHTML = days + "d " + hours + "h " + minutes + "m";
-        // If the count down is finished, write some text
+        divTag.innerHTML = days + "d " + hours + "h " + minutes + "m";
         if (distance < 0) {
             clearInterval(x);
-            alertFuncForBur();
+            (function () {
+                alert(textToAlert);
+                TimerWithAlert(divTag, milisecondsToRun, textToAlert);
+            })();
         }
     }, 1000);
 }
-function myStartFunctionForMad() {
-    // Set the date we're counting down to
-    var countDownDate = new Date().getTime() + 604800000; // Det sidste er en uge i milisekunder
-    // Update the count down every 1 second
+function autoReloadData() {
+    var countDownDate = new Date().getTime() + 10000;
     var x = setInterval(function () {
-        // Get today's date and time
         var now = new Date().getTime();
-        // Find the distance between now and the count down date
         var distance = countDownDate - now;
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        // Display the result in the element with id="demo"
-        contentOfTimerMad.innerHTML = days + "d " + hours + "h " + minutes + "m";
-        // If the count down is finished, write some text
-        if (distance < 0) {
-            clearInterval(x);
-            alertFuncForMad();
-        }
-    }, 1000);
-}
-function autoReloadVideoData() {
-    // Set the date we're counting down to
-    var countDownDate = new Date().getTime() + 10000; // Det sidste er en uge i milisekunder
-    // Update the count down every 1 second
-    var x = setInterval(function () {
-        // Get today's date and time
-        var now = new Date().getTime();
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
             showAllTimeStamps();
             showWeather();
-            autoReloadVideoData();
+            autoReloadData();
         }
     }, 1000);
 }
 function showAllTimeStamps() {
     _node_modules_axios_index__WEBPACK_IMPORTED_MODULE_0___default.a.get(baseUri)
         .then(function (response) {
-        printDataToAllTimeStampsDiv(response.data);
+        videoLogs.innerHTML = "";
+        var result = "";
+        response.data.forEach(function (record) {
+            result += "#" + record.id + ": " + record.time + "<br>";
+            videoLogs.innerHTML = result;
+        });
     })
         .catch(function (error) {
         if (error.response) {
             videoLogs.innerHTML = error.message;
         }
-        else { // something went wrong in the .then block?
+        else {
             videoLogs.innerHTML = error.message;
         }
-    });
-}
-function printDataToAllTimeStampsDiv(records) {
-    videoLogs.innerHTML = "";
-    var result = "";
-    records.forEach(function (record) {
-        result += "#" + record.id + ": " + record.time + "<br>";
-        videoLogs.innerHTML = result;
     });
 }
 function showWeather() {
